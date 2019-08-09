bc6624201043a3e042580175a02d9758824f0476
vatsalsonigara <vatsalsonigara@gmail.com>
2019-08-09 23:38:46 +0530

    changed register file

--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -14,38 +14,43 @@ export class Register extends React.Component {
           <div className="image">
             <img src={loginImg} />
           </div>
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="Username" />
+          <form action="http://localhost:8000/user/register" method="POST">
+
+          
+            <div className="form">
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" placeholder="Username" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" placeholder="Name" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="dob">Date Of Birth</label>
+                <input type="text" name="dob" placeholder="YYYY-MM-DD" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="college">College/Company</label>
+                <input type="text" name="college" placeholder="College/Company" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" placeholder="Email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="text" name="password" placeholder="Password" />
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">Name</label>
-              <input type="text" name="password" placeholder="Name" />
+            <div className="footer">
+              <button type="submit" className="btn">
+                Register
+              </button>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">Date Of Birth</label>
-              <input type="text" name="password" placeholder="YYYY-MM-DD" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">College/Company</label>
-              <input type="text" name="password" placeholder="College/Company" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="Email" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="Password" />
-            </div>
-          </div>
-        </div>
-        <div className="footer">
-          <button type="button" className="btn">
-            Register
-          </button>
-        </div>
+          </form>
+      </div>
+        
       </div>
     );
   }
